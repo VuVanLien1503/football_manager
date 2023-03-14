3c46ab989529e77ac3e2a95506c5b99b7629392d
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2023-03-14 16:36:06 +0700

    Tạo admin controller, hiển thị typicalCoach thành công

--- template/coach_ajax/coach.js
+++ template/coach_ajax/coach.js
@@ -27,3 +27,31 @@
             }
         })
     }
+    function displayTypicalCoachAdmin(){
+        $.ajax({
+            url : "http://localhost:8081/admin/coaches/typical",
+            type: "GET",
+            success(data){
+                let context = ""
+                for (let i = 0; i < data.length; i++) {
+                    context += `<div class="col-sm-12 col-md-4">
+                                    <div class="coach-item">
+                                      <div class="gambar">
+                                        <img src="${data[i].imagePath}" style="width: 500px; height: 500px" alt="Coach" class="img-responsive">
+                                      </div>
+                                      <div class="item-body">
+                                        <div class="name">
+                                          ${data[i].name}
+                                        </div>
+                                        <div class="position">
+                                          ${data[i].position.name}
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>`
+                }
+                document.getElementById("typicalCoach").innerHTML = context;
+
+            }
+        })
+    }
