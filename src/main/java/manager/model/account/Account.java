5a4f750d09d09af81d1a620b7eae98f9cac34d49
PhamDucCuong1991 <cuongvnb@gmail.com>
2023-03-14 14:25:01 +0700

    Theem config sercurity

--- src/main/java/manager/model/account/Account.java
+++ src/main/java/manager/model/account/Account.java
@@ -19,7 +19,7 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    @UniqueElements
+    @Column(unique = true)
     private String username;
     @NotNull
     private String password;
